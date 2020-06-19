4bfef8e8284feda1428b9f74ece8e8fe2ac5ae34
Thaynan Saveli <thay.henze@hotmail.com>
2020-06-19 19:43:22 -0300

    monster-slayer v0

---
+++ desafio-05/app.js
@@ -0,0 +1,17 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		nomes: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			Titulo: 'O Senhor dos Anéis',
+			Autor: 'J.R.R. Tolkiens',
+			Volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		visualizar: true
+	}
+});
